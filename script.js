5e4b6c2b8f158fb3aa1bcdbf044f59ca43e62291
CaroKris <ckhilpisch@gmail.com>
2021-01-13 23:08:44 -0600

    changes

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ $(function () {
   $startButton = $("#startButton");
   $answers = $("ul");
   $answersArea = $('.choice')
-  $choiceA = $("#A");
-  $choiceB = $("#B");
-  $choiceC = $("#C");
-  $choiceD = $("#D");
+  $ans1 = $("#ans1");
+  $ans2 = $("#ans2");
+  $ans3 = $("#ans3");
+  $ans4 = $("#ans4");
   $question = $("#question");
   $timer = $("#timer");
   $highScore = $("#highscore");
@@ -20,8 +20,8 @@ $(function () {
     $question.show();
     $("ul").show();
     $timer.show();
-    renderQuestion();
-    // $timer.show();
+    quest0();
+
     $counter = 90;
     $interval = setInterval(function () {
       $counter--;
@@ -35,213 +35,139 @@ $(function () {
     }, 1000);
   });
 
-
-    questions = [
-        {
-            question: 'In the UK, the abrreviation NHS stands for National what Service?',
-            choiceA: 'Humanity',
-            choiceB: 'Health',
-            choiceC: 'Honour',
-            choiceD: 'Household',
-            correct: 'B', 
-        },
-        {
-            question: 'Which Disney character famously leaves behind a glass slipper at a royal ball?',
-            choiceA: 'Pocahontas',
-            choiceB: 'Sleeping Beauty',
-            choiceC: 'Cinderella',
-            choiceD: 'Elsa',
-            correct: 'C',    
-        },
-        {
-            question: 'What does the word loquacious mean?',
-            choiceA: 'Angry',
-            choiceB: 'Chatty',
-            choiceC: 'Beautiful',
-            choiceD: 'Shy',
-            correct: 'B',    
-        },
-        {
-            question: 'The hammer and sickle is one of the most recognizeable symbols of which political ideology?',
-            choiceA: 'Republicanism',
-            choiceB: 'Communism',
-            choiceC: 'Liberalism',
-            choiceD: 'Conservativism',
-            correct: 'B',     
-        },
-        {
-            question: 'Which childrens toys have been marketed with the phrase "robots in disguise"?',
-            choiceA: 'Bratz Dolls',
-            choiceB: 'Sylvanian Families',
-            choiceC: 'Hatchimals',
-            choiceD: 'Transformers',
-            correct: 'D',   
-        },
-        {
-            question: 'Construction of which of these famous landmarks was completed first?',
-            choiceB: 'Royal Albert Hall',
-            choiceC: 'The Eiffel Tower',
-            choiceD: 'Big Ben Clock Tower',
-            correct: 'D',    
-        },
-    ]
-
-    var finalQuestion = questions.length - 1;
-    var runningQuestion = 0;
-
-    function renderQuestion(){
-        var q = questions[runningQuestion];
-        $question.html = q.question;
-        $choiceA.html = q.choiceA;
-        $choiceB.html = q.choiceB;
-        $choiceC.html = q.choiceC;
-        $choiceD.html = q.choiceD;
-    }
-    function checkanswer (answer) {
-        if (questions[runningQuestionIndex].correct ==answer)
-        answerIsCorrect();
-        } else {
-        answerIsIncorrect();
-        }
-
+  function quest0() {
+    $question.html(
+      "In the UK, the abrreviation NHS stands for National what Service?"
+    );
+    $ans1.html("Humanity");
+    $ans2.html("Health");
+    $ans2.removeClass("incorrect").addClass("correct");
+    $ans3.html("Honour");
+    $ans4.html("Household");
+  
+    $(".correct").on("click", function () {
+      $(".correct,.incorrect").off();
+      $ans2.removeClass("correct").addClass("incorrect");
+      quest1();
+    });
+  
+    $(".incorrect").on("click", function () {
+      console.log("decrement timer");
+    });
+  }
+  
+  function quest1() {
+    $question.html(
+      "Which Disney character famously leaves behind a glass slipper at a royal ball?"
+    );
+    $ans1.html("Pocahontas");
+    $ans2.html("Sleeping Beauty");
+    $ans3.html("Cinderella");
+    $ans3.removeClass("incorrect").addClass("correct");
+    $ans4.html("Elsa");
+  
+    $(".correct").on("click", function () {
+      $(".correct,.incorrect").off();
+      $ans3.removeClass("correct").addClass("incorrect");
+  
+      quest2();
+    });
+  
+    $(".incorrect").on("click", function () {
+      console.log("decrement timer");
+    });
+  }
+  
+  function quest2() {
+    $question.html("What does the word loquacious mean?");
+    $ans1.html("Angry");
+    $ans2.html("Chatty");
+    $ans2.removeClass("incorrect").addClass("correct");
+    $ans3.html("Beautiful");
+    $ans4.html("Shy");
+  
+    $(".correct").on("click", function () {
+      $(".correct,.incorrect").off();
+      $ans2.removeClass("correct").addClass("incorrect");
+      quest3();
+    });
+  
+    $(".incorrect").on("click", function () {
+      console.log("decrement timer");
+    });
+  }
+  
+  function quest3() {
+    $question.html(
+      "The hammer and sickle is one of the most recognizeable symbols of which political ideology?"
+    );
+    $ans1.html("Rebublicanism");
+    $ans2.html("Communism");
+    $ans2.removeClass("incorrect").addClass("correct");
+    $ans3.html("Conservatism");
+    $ans4.html("Liberalism");
+  
+    $(".correct").on("click", function () {
+      $(".correct,.incorrect").off();
+      $ans2.removeClass("correct").addClass("incorrect");
+      quest4();
+    });
+  
+    $(".incorrect").on("click", function () {
+      console.log("decrement timer");
+    });
+  }
+  
+  function quest4() {
+    $question.html(
+      'Which childrens toys have been marketed with the phrase "robots in disguise"?'
+    );
+    $ans1.html("Bratz Dolls");
+    $ans2.html("Sylvanian Families");
+    $ans3.html("Hatchimals");
+    $ans4.html("Transformers");
+    $ans4.removeClass("incorrect").addClass("correct");
+  
+    $(".correct").on("click", function () {
+      $(".correct,.incorrect").off();
+      $ans4.removeClass("correct").addClass("incorrect");
+      quest5();
+    });
+  
+    $(".incorrect").on("click", function () {
+      console.log("decrement timer");
+    });
+  }
+  
+  function quest5() {
+    $question.html(
+      "Construction of which of these famous landmarks was completed first?"
+    );
+    $ans1.html("The Empire State Building");
+    $ans2.html("Royal Albert Hall");
+    $ans3.html("The Eiffel Tower");
+    $ans4.html("Big Ben Clock Tower");
+    $ans4.removeClass("incorrect").addClass("correct");
+  
+    $(".correct").on("click", function () {
+      $(".correct,.incorrect").off();
+      $ans4.removeClass("correct").addClass("incorect");
+      endquiz();
+    });
+  
+    $(".incorrect").on("click", function () {
+      console.log("decrement timer");
+    });
+  }
+  
+  function endquiz() {
+    $("ul").hide();
+    $question.hide();
+    $gameOver.html("GAME OVER!!!!  YOUR SCORE IS " + $counter + " !!!!");
+    $yourScore = $counter;
     
-
-    renderQuestion();
-    runningQuestion++
-    renderQuestion();
-
-});
-// function quest0() {
-//   $question.html(
-//     "In the UK, the abrreviation NHS stands for National what Service?"
-//   );
-//   $ans1.html("Humanity");
-//   $ans2.html("Health");
-//   $ans2.removeClass("incorrect").addClass("correct");
-//   $ans3.html("Honour");
-//   $ans4.html("Household");
-
-//   $(".correct").on("click", function () {
-//     $(".correct,.incorrect").off();
-//     $ans2.removeClass("correct").addClass("incorrect");
-//     quest1();
-//   });
-
-//   $(".incorrect").on("click", function () {
-//     console.log("decrement timer");
-//   });
-// }
-
-// function quest1() {
-//   $question.html(
-//     "Which Disney character famously leaves behind a glass slipper at a royal ball?"
-//   );
-//   $ans1.html("Pocahontas");
-//   $ans2.html("Sleeping Beauty");
-//   $ans3.html("Cinderella");
-//   $ans3.removeClass("incorrect").addClass("correct");
-//   $ans4.html("Elsa");
-
-//   $(".correct").on("click", function () {
-//     $(".correct,.incorrect").off();
-//     $ans3.removeClass("correct").addClass("incorrect");
-
-//     quest2();
-//   });
-
-//   $(".incorrect").on("click", function () {
-//     console.log("decrement timer");
-//   });
-// }
-
-// function quest2() {
-//   $question.html("What does the word loquacious mean?");
-//   $ans1.html("Angry");
-//   $ans2.html("Chatty");
-//   $ans2.removeClass("incorrect").addClass("correct");
-//   $ans3.html("Beautiful");
-//   $ans4.html("Shy");
-
-//   $(".correct").on("click", function () {
-//     $(".correct,.incorrect").off();
-//     $ans2.removeClass("correct").addClass("incorrect");
-//     quest3();
-//   });
-
-//   $(".incorrect").on("click", function () {
-//     console.log("decrement timer");
-//   });
-// }
-
-// function quest3() {
-//   $question.html(
-//     "The hammer and sickle is one of the most recognizeable symbols of which political ideology?"
-//   );
-//   $ans1.html("Rebublicanism");
-//   $ans2.html("Communism");
-//   $ans2.removeClass("incorrect").addClass("correct");
-//   $ans3.html("Conservatism");
-//   $ans4.html("Liberalism");
-
-//   $(".correct").on("click", function () {
-//     $(".correct,.incorrect").off();
-//     $ans2.removeClass("correct").addClass("incorrect");
-//     quest4();
-//   });
-
-//   $(".incorrect").on("click", function () {
-//     console.log("decrement timer");
-//   });
-// }
-
-// function quest4() {
-//   $question.html(
-//     'Which childrens toys have been marketed with the phrase "robots in disguise"?'
-//   );
-//   $ans1.html("Bratz Dolls");
-//   $ans2.html("Sylvanian Families");
-//   $ans3.html("Hatchimals");
-//   $ans4.html("Transformers");
-//   $ans4.removeClass("incorrect").addClass("correct");
-
-//   $(".correct").on("click", function () {
-//     $(".correct,.incorrect").off();
-//     $ans4.removeClass("correct").addClass("incorrect");
-//     quest5();
-//   });
-
-//   $(".incorrect").on("click", function () {
-//     console.log("decrement timer");
-//   });
-// }
-
-// function quest5() {
-//   $question.html(
-//     "Construction of which of these famous landmarks was completed first?"
-//   );
-//   $ans1.html("The Empire State Building");
-//   $ans2.html("Royal Albert Hall");
-//   $ans3.html("The Eiffel Tower");
-//   $ans4.html("Big Ben Clock Tower");
-//   $ans4.removeClass("incorrect").addClass("correct");
-
-//   $(".correct").on("click", function () {
-//     $(".correct,.incorrect").off();
-//     $ans4.removeClass("correct").addClass("incorect");
-//     endquiz();
-//   });
-
-//   $(".incorrect").on("click", function () {
-//     console.log("decrement timer");
-//   });
-// }
-
-// function endquiz() {
-//   $("ul").hide();
-//   $question.hide();
-//   $gameOver.html("GAME OVER!!!!  YOUR SCORE IS " + $counter + " !!!!");
-
-// }
+  }
+  
 
 // // $(function(class) {
 // //     if (class == correct), {
@@ -263,4 +189,5 @@ $(function () {
 
 // // repeat until finished
 // // show scorereceived
-// // option to add intials for highscore
+// // option to add intials //
+  });
